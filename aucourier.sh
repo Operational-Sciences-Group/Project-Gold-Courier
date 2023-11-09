93f8b0b78ca20430c935300c8446fd378a395e23
TheDiodeAdvocate <80556845+TheDiodeAdvocate@users.noreply.github.com>
2023-11-09 13:23:08 -0500

    Update aucourier.sh
    Added no rules for removable media, corrected issues with auditctl only adding rules temporarily.

--- aucourier.sh
+++ aucourier.sh
@@ -16,7 +16,7 @@ filedate=$(date +"%m_%d_%Y")
 clear
 
 # Takes user input to determine audit 
-printf "${yellow}Please select from the following options:${clear} \n1)Full Audit \n2)Audit Summary \n3)Audit Rules Report \n4)Add auditing rules \n"
+printf "${yellow}Please select from the following options:${clear} \n1)Full Audit \n2)Audit Summary \n3)Audit Rules Report \n4)Add Auditing Rules \n"
 read audit_level
 
 if [[ $audit_level = 1 ]]; then
@@ -59,17 +59,21 @@ if [[ $audit_level = 1 ]]; then
     echo -e "${green}Checking for ${red}deleted users... ${clear}"
     ausearch -i --start $start_date --end $end_date -m DEL_USER
 
-    # Check for possible password tampering (requires audit rule 1 below)
-    echo -e "${green}Checking for possible password changes... ${clear}"
-    ausearch -i -k passwd_changes
+    # Check for removable media
+    echo -e "${green}Checking for the use of removable media... ${clear}"
+    ausearch -i --start $start_date --end $end_date -k removable-media
 
-    # Check for new directories/links etc
+    # Check for possible password tampering (requires audit rule 1 below)
+    echo -e "${green}Zipping possible password changes... ${clear}"
+    ausearch -i --start $start_date --end $end_date -k passwd_changes | gzip > PWChanges_"${filedate}".gz
+
+    # Prints created and new directories/links etc
     echo -e "${green}Zipping created directories and links to file... ${clear}" 
-    ausearch -k creation | gzip > CreationAudit_"${filedate}".gz
+    ausearch --start $start_date --end $end_date -k creation | gzip > CreationAudit_"${filedate}".gz
 
     # Prints commands to file for grep/audit
     echo -e "${green}Zipping executed commands to file... ${clear}"
-    ausearch -k allcmds | gzip > CommandAudit_"${filedate}".gz
+    ausearch --start $start_date --end $end_date -k allcmds | gzip > CommandAudit_"${filedate}".gz
 
 elif [[ $audit_level = 2 ]]; then
     # Prompt user for date range
@@ -90,18 +94,29 @@ elif [[ $audit_level = 3 ]]; then
 elif [[ $audit_level = 4 ]]; then
     # Clear screen
     clear
-    # Adds special audit rules to system
-    printf "${yellow}Please select from the following options:${clear} \n1)Enable audit for password changes \n2)Enable auditing for all commands \n"
+    # Adds special audit rules to system. Add rule and use Reload Rule to load it perm. 
+    printf "${yellow}Please select from the following options:${clear} \n1)Enable audit for password changes \n2)Enable audit for all commands \n3)Enable audit for removable media \nReload Rules \n"
     
     read rule
 
         if [[ $rule = 1 ]]; then 
-            auditctl -w /etc/passwd -p wa -k passwd_changes
+            # Should not be needed if using NISPOM Rules 
+            echo "-w /etc/shadow -p w -k passwd_changes" >> /etc/audit/rules.d/audit.rules
 
         elif [[ $rule = 2 ]]; then 
-            auditctl -a always,exit -F arch=b32 -S execve -F key=allcmds
-            auditctl -a always,exit -F arch=b64 -S execve -F key=allcmds
+            # Audits commands
+            echo "-a always,exit -F arch=b32 -S execve -F key=allcmds" >> /etc/audit/rules.d/audit.rules
+            echo "-a always,exit -F arch=b64 -S execve -F key=allcmds" >> /etc/audit/rules.d/audit.rules
+
+	    elif [[ $rule = 3 ]]; then
+            # Audits removable media
+            echo "-a always,exit -F arch=b64 -S mount -S umount2 -k removable-media" >> /etc/audit/rules.d/audit.rules
+	
         
+        elif [[ $rule == "Reload Rules" ]]; then
+            # Puts rules into a perm state, check using rules report above (auditctl -l)
+            augenrules --load
+
         else
             # Fails out of rules addition
             echo -e "${red}Invalid Choice. Please enter a number between 1 and 2.${clear}"
